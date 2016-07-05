aff6a2e9a0d8aed1b27b6960ea89c8f75be1ca66
ming <mujinye@gmail.com>
2016-07-05 17:02:04 +0800

    baidu

--- app/src/main/java/com/example/lovem/networktest/MainActivity.java
+++ app/src/main/java/com/example/lovem/networktest/MainActivity.java
@@ -14,65 +14,66 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-    public static final int SHOW_RESPONSE = 0;
+public class MainActivity extends Activity implements View.OnClickListener{
+    public static final int SHOW_RESPONSE=0;
     private Button sendRequest;
-
     private TextView responseText;
-    private Handler handler = new Handler() {
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
+
+    private Handler handler=new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
                 case SHOW_RESPONSE:
-                    String response = (String) msg.obj;
-// 在这里进行UI操作，将结果显示到界面上
+                    String response=(String) msg.obj;
                     responseText.setText(response);
             }
         }
     };
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sendRequest = (Button) findViewById(R.id.send_request);
-        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest=(Button)findViewById(R.id.send_request);
+        responseText=(TextView) findViewById(R.id.response_text);
         sendRequest.setOnClickListener(this);
     }
+
     @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.send_request) {
-            sendRequestWithHttpURLConnection();
+    public void onClick(View view){
+        if(view.getId()==R.id.send_request){
+            sendRequestWithHttpUrlConnection();
         }
     }
-    private void sendRequestWithHttpURLConnection() {
-// 开启线程来发起网络请求
+
+    private void sendRequestWithHttpUrlConnection(){
         new Thread(new Runnable() {
             @Override
             public void run() {
-                HttpURLConnection connection = null;
-                try {
-                    URL url = new URL("http://www.baidu.com");
-                    connection = (HttpURLConnection) url.openConnection();
+                HttpURLConnection connection=null;
+                try{
+                    URL url =new URL("http://www.baidu.com");
+                    connection=(HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
                     connection.setConnectTimeout(8000);
                     connection.setReadTimeout(8000);
-                    InputStream in = connection.getInputStream();
-// 下面对获取到的输入流进行读取
-                    BufferedReader reader = new BufferedReader(new
+                    InputStream in=connection.getInputStream();
+                    BufferedReader reader=new BufferedReader(new
                             InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
+                    StringBuilder response=new StringBuilder();
                     String line;
-                    while ((line = reader.readLine()) != null) {
+                    while((line=reader.readLine())!=null){
                         response.append(line);
                     }
-                    Message message = new Message();
-                    message.what = SHOW_RESPONSE;
-// 将服务器返回的结果存放到Message中
-                    message.obj = response.toString();
+                    Message message=new Message();
+                    message.what=SHOW_RESPONSE;
+
+                    message.obj=response.toString();
                     handler.sendMessage(message);
-                } catch (Exception e) {
+
+                }catch (Exception e){
                     e.printStackTrace();
-                } finally {
-                    if (connection != null) {
+                }finally {
+                    if(connection!=null){
                         connection.disconnect();
                     }
                 }
@@ -131,4 +132,4 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
 
 
-}
+
