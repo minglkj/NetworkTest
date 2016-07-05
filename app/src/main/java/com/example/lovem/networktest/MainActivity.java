70b58b8cd2b770417abdd9369191589349bcf05a
ming <mujinye@gmail.com>
2016-07-05 16:46:19 +0800

    something wrong

--- app/src/main/java/com/example/lovem/networktest/MainActivity.java
+++ app/src/main/java/com/example/lovem/networktest/MainActivity.java
@@ -1,13 +1,135 @@
 package com.example.lovem.networktest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+    public static final int SHOW_RESPONSE=0;
+    private Button sendRequest;
+    private TextView responseText;
+
+    private Handler handler=new Handler(){
+        public void handleMessage(Message msg){
+            switch (msg.what){
+                case SHOW_RESPONSE:
+                    String response=(String) msg.obj;
+                    responseText.setText(response);
+            }
+        }
+    };
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendRequest=(Button)findViewById(R.id.send_request);
+        responseText=(TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view){
+        if(view.getId()==R.id.send_request){
+            sendRequestWithHttpUrlConnection();
+        }
+    }
+
+    private void sendRequestWithHttpUrlConnection(){
+        new Thread((new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection=null;
+                try{
+                    URL url =new URL("http://www.weibo.com");
+                    connection=(HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in=connection.getInputStream();
+                    BufferedReader reader=new BufferedReader(new
+                            InputStreamReader(in));
+                    StringBuilder response=new StringBuilder();
+                    String line;
+                    while((line=reader.readLine())!=null){
+                        response.append(line);
+                    }
+                    Message message=new Message();
+                    message.what=SHOW_RESPONSE;
+
+                    message.obj=response.toString();
+                    handler.sendMessage(message);
+
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(connection!=null){
+                        connection.disconnect();
+                    }
+                }
+            }
+        })).start();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
